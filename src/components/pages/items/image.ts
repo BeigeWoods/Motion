e4c5bfd689d6cd942447d0e953ea9ed15870db87
이주현 <leejuhyun@ijuhyeons-MacBook-Pro.local>
2022-01-05 16:40:51 +0900

    refactoring components

--- src/components/pages/items/image.ts
+++ src/components/pages/items/image.ts
@@ -1,15 +1,13 @@
-export class ImageComponent {
-  private element: HTMLElement;
+import { BaseComponent } from "../../base.js";
+
+export class ImageComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
-    const template = document.createElement("template");
-    template.innerHTML = `
-    <section class="image">
-  <div class="image_holder">
-    <img class="image_thumbnail">
-  </div>
-  <p class="image_title"></p>
-</section>`;
-    this.element = template.content.firstElementChild! as HTMLElement;
+    super(`<section class="image">
+              <div class="image_holder">
+                <img class="image_thumbnail">
+              </div>
+              <p class="image_title"></p>
+          </section>`);
 
     const imageElement = this.element.querySelector(
       ".image_thumbnail"
@@ -21,9 +19,5 @@ export class ImageComponent {
       ".image_title"
     )! as HTMLParagraphElement;
     titleElement.textContent = title;
-  }
-
-  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
-    parent.insertAdjacentElement(position, this.element);
   }
 }
