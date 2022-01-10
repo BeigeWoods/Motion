c022118e9d0198466e4b3b82f1b858489863cd9c
이주현 <leejuhyun@ijuhyeons-MacBook-Pro.local>
2022-01-10 23:22:51 +0900

    add dialog input

--- src/app.ts
+++ src/app.ts
@@ -8,28 +8,68 @@ import { VideoComponent } from "./components/pages/items/video.js";
 import { NoteComponent } from "./components/pages/items/note.js";
 import { TodoComponent } from "./components/pages/items/todo.js";
 import { Component } from "./components/base.js";
+import {
+  InputDialog,
+  MediaData,
+  TextData,
+} from "./components/dialog/dialog.js";
+import { MediaSectionInput } from "./components/dialog/input/media-input.js";
+import { TextSectionInput } from "./components/dialog/input/text-input.js";
+
+type InputComponentConstructor<T = (MediaData | TextData) & Component> = {
+  new (): T;
+};
 
 class App {
   private readonly page: Component & Composable;
-  constructor(appRoot: HTMLElement) {
+  constructor(appRoot: HTMLElement, private dialogRoot: HTMLElement) {
     this.page = new PageComponent(PageItemComponent);
     this.page.attachTo(appRoot);
 
-    const image = new ImageComponent("shine", "https://picsum.photos/600/300");
-    this.page.addChild(image);
-
-    const video = new VideoComponent(
-      "music",
-      "https://www.youtube.com/watch?v=X4Ojh53pJRg"
+    this.bindElementToDialog<MediaSectionInput>(
+      "#new-image",
+      MediaSectionInput,
+      (input: MediaSectionInput) => new ImageComponent(input.title, input.url)
     );
-    this.page.addChild(video);
+    this.bindElementToDialog<MediaSectionInput>(
+      "#new-video",
+      MediaSectionInput,
+      (input: MediaSectionInput) => new VideoComponent(input.title, input.url)
+    );
+    this.bindElementToDialog<TextSectionInput>(
+      "#new-note",
+      TextSectionInput,
+      (input: TextSectionInput) => new NoteComponent(input.title, input.body)
+    );
+    this.bindElementToDialog<TextSectionInput>(
+      "#new-todo",
+      TextSectionInput,
+      (input: TextSectionInput) => new TodoComponent(input.title, input.body)
+    );
+  }
 
-    const note = new NoteComponent("Note", "hellow");
-    this.page.addChild(note);
+  private bindElementToDialog<T extends (MediaData | TextData) & Component>(
+    selector: string,
+    inputComponent: InputComponentConstructor<T>,
+    makeSection: (input: T) => Component
+  ) {
+    const button = document.querySelector(selector)! as HTMLButtonElement;
+    button.addEventListener("click", () => {
+      const dialog = new InputDialog();
+      const input = new inputComponent();
+      dialog.addChild(input);
+      dialog.attachTo(this.dialogRoot);
 
-    const todo = new TodoComponent("Todo", "what to do");
-    this.page.addChild(todo);
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(this.dialogRoot);
+      });
+      dialog.setOnSubmitListener(() => {
+        const media = makeSection(input);
+        this.page.addChild(media);
+        dialog.removeFrom(this.dialogRoot);
+      });
+    });
   }
 }
 
-new App(document.querySelector(".document")! as HTMLElement);
+new App(document.querySelector(".document")! as HTMLElement, document.body);
---
+++ src/components/dialog/dialog.ts
@@ -0,0 +1,56 @@
+import { BaseComponent, Component } from "../base.js";
+import { Composable } from "../pages/page.js";
+
+type OnCloseListener = () => void;
+type OnSubmitListener = () => void;
+
+export interface MediaData {
+  readonly title: string;
+  readonly url: string;
+}
+
+export interface TextData {
+  readonly title: string;
+  readonly body: string;
+}
+
+export class InputDialog
+  extends BaseComponent<HTMLElement>
+  implements Composable
+{
+  private closeListener?: OnCloseListener;
+  private submitListener?: OnSubmitListener;
+  constructor() {
+    super(`<dialog class="dialog">
+            <div class="dialog_container">
+              <button class="close">&times;</button>
+              <div id="dialog_body"></div>
+              <button type="submit" form="dialog-form" class="dialog_submit">ADD</button>
+            </div>
+          </dialog>`);
+    const closeBtn = this.element.querySelector(".close")! as HTMLButtonElement;
+    const addBtn = this.element.querySelector(
+      ".dialog_submit"
+    )! as HTMLButtonElement;
+
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
+    addBtn.onclick = () => {
+      this.submitListener && this.submitListener();
+    };
+  }
+
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
+  }
+
+  setOnSubmitListener(listener: OnSubmitListener) {
+    this.submitListener = listener;
+  }
+
+  addChild(child: Component) {
+    const body = this.element.querySelector("#dialog_body")! as HTMLElement;
+    child.attachTo(body);
+  }
+}
